4a5f5f34284fcd5726c62ade067ab4f9ff9f7b7d
Philip-Machar <philipmachar788@gmail.com>
2025-04-09 23:55:30 +0300

    Initial commit

--- script.js
+++ script.js
@@ -22,3 +22,39 @@ class Player {
 
 const player = new Player((canvas.width/2), (canvas.height/2), 20, 'blue');
 player.draw();
+
+class Projecttile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    };
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+    };
+
+    update() {
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    };
+};
+
+const projectile = new Projecttile(canvas.width/2, canvas.height/2, 5, 'red', {x: 1, y: 1});
+    projectile.draw();
+    projectile.update();
+
+const animate = () => {
+    requestAnimationFrame(animate);
+    projectile.draw();
+    projectile.update();
+}
+
+canvas.addEventListener('click', () => {
+    animate();
+});
